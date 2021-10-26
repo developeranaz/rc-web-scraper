530acd3ea2eb596954305518f402745f713307b2
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-10-26 22:13:08 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt install curl -y
 IDPATH=$(which curl|sed 's/\/curl//g')
-curl 'https://github.com/developeranaz/rc-web-scraper/blob/main/rcscraper?raw=true' >$IDPATH/rcscraper
+curl 'https://raw.githubusercontent.com/developeranaz/rc-web-scraper/main/rcscraper' >$IDPATH/rcscraper
 echo 'I think rcscraper is installed on your device '
 echo 'Try using --> rcscraper -u "https://your-scrapuri"'
